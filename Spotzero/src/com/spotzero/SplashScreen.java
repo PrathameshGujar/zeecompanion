670ac5a8691f20c0c1b348851d64f7d2400fd9ae
prathamesh <prathamesh@g5web.com>
2015-07-18 16:45:09 +0530

    Splash Screen Added

---
+++ Spotzero/src/com/spotzero/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.spotzero;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(SplashScreen.this,HomeActivity.class));
+			}
+		}, 3000);
+	}
+
+}
